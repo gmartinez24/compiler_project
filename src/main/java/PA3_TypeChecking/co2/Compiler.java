2d5bd1d24848a254021576124b96b3c1360cc6b0
Darwin Mohajeri <darwinmohajeri@gmail.com>
2024-02-21 16:51:33 -0600

    Moved files to PA3 folder, copied parser into compiler

--- src/main/java/PA3_TypeChecking/co2/Compiler.java
+++ src/main/java/PA3_TypeChecking/co2/Compiler.java
@@ -206,17 +206,286 @@ public class Compiler {
         return expectRetrieve(nt);
     }
 
-    // TODO: copy operators and type grammar rules from Compiler
 
     // literal = integerLit | floatLit
     private Token literal () {
         return matchNonTerminal(NonTerminal.LITERAL);
     }
 
-    // TODO: copy remaining grammar rules from Compiler and make edits to build ast
+
+    // designator = ident { "[" relExpr "]" }
+    private void designator () {
+        int lineNum = lineNumber();
+        int charPos = charPosition();
+
+        Token ident = expectRetrieve(Token.Kind.IDENT);
+        while (accept(Token.Kind.OPEN_BRACKET)) {
+            relExpr();
+            accept(Token.Kind.CLOSE_BRACKET);
+        }
+
+    }
+
 
     // computation	= "main" {varDecl} {funcDecl} "{" statSeq "}" "."
-    private Computation computation () {
-        throw new RuntimeException("implement all grammar rules to build ast");
+    private void computation () {
+
+        expect(Token.Kind.MAIN);
+
+        // deal with varDecl 0 or many
+        while (have(NonTerminal.VAR_DECL)) {
+            varDecl();
+        }
+
+        // deal with funcDecl 0 or many
+        while (have(NonTerminal.FUNC_DECL)) {
+            funcDecl();
+        }
+
+        expect(Token.Kind.OPEN_BRACE);
+        statSeq();
+        expect(Token.Kind.CLOSE_BRACE);
+        expect(Token.Kind.PERIOD);
     }
+
+    // varDecl = typeDecl ident {"," ident} ";"
+    private void varDecl() {
+        typeDecl();
+        do {
+            expect(Token.Kind.IDENT);
+        } while(accept(Token.Kind.COMMA));
+        expect(Token.Kind.SEMICOLON);
+    }
+
+    // typeDecl = type  { "[" integerLit "]" }
+    private void typeDecl() {
+        expect(NonTerminal.TYPE_DECL);
+        while (accept(Token.Kind.OPEN_BRACKET)) {
+            expect(Token.Kind.INT_VAL);
+            expect(Token.Kind.CLOSE_BRACKET);
+        }
+//        } else {
+//            // may need to change this to reflect something other than expecting INT
+//            expect(Token.Kind.INT);
+//        }
+
+
+    }
+
+    // funcDecl = "function" ident formalParam ":" ("void" | type ) funcBody
+    private void funcDecl() {
+        expect(Token.Kind.FUNC);
+        expect(Token.Kind.IDENT);
+        formalParam();
+        expect(Token.Kind.COLON);
+
+        // searches for either a VOID or a TYPE
+        if (!(accept(Token.Kind.VOID) || accept(NonTerminal.TYPE_DECL))) {
+            // may need to change this to reflect something other than expecting VOID
+            expect(NonTerminal.FUNC_DECL);
+        }
+
+        funcBody();
+    }
+
+    // formalParam = "(" [paramDecl { "," paramDecl}] ")"
+    private void formalParam() {
+        expect(Token.Kind.OPEN_PAREN);
+        if (have(NonTerminal.PARAM_DECL)){
+            do {
+                paramDecl();
+            } while(accept(Token.Kind.COMMA));
+        }
+        expect(Token.Kind.CLOSE_PAREN);
+
+    }
+
+    // paramDecl = paramType ident
+    private void paramDecl() {
+        paramType();
+        expect(Token.Kind.IDENT);
+    }
+
+    // paramType = type {"[" "]"}
+    private void paramType() {
+        expect(NonTerminal.TYPE_DECL);
+        while (accept(Token.Kind.OPEN_BRACKET)) {
+            expect(Token.Kind.CLOSE_BRACKET);
+        }
+    }
+
+    // funcBody = "{" {varDecl}  statSeq "}" ";"
+    private void funcBody() {
+        expect(Token.Kind.OPEN_BRACE);
+        while (have(NonTerminal.VAR_DECL)) {
+            varDecl();
+        }
+
+        statSeq();
+
+        expect(Token.Kind.CLOSE_BRACE);
+        expect(Token.Kind.SEMICOLON);
+    }
+
+    // statSeq = statement ";" { statement ";" }
+    private void statSeq() {
+        do{
+            statement();
+            expect(Token.Kind.SEMICOLON);
+        } while (have(NonTerminal.STATEMENT));
+
+    }
+
+    // statement = assign | funcCall | ifStat | whileStat | repeatStat | returnStat
+    private void statement() {
+        // assign throws an error on erroneous function call (function call without "call" keyword)
+        if (have(NonTerminal.ASSIGN)) {
+            assign();
+        }
+        else if (have(NonTerminal.FUNC_CALL)) {
+            funcCall();
+        }
+        else if (have(NonTerminal.IF_STAT)) {
+            ifStat();
+        }
+        else if (have(NonTerminal.WHILE_STAT)) {
+            whileStat();
+        }
+        else if (have(NonTerminal.REPEAT_STAT)) {
+            repeatStat();
+        }
+        else if (have(NonTerminal.RETURN_STAT)) {
+            returnStat();
+        } else {
+            expect(NonTerminal.STATEMENT);
+        }
+
+    }
+
+    // assign = designator ( ( assignOp relExpr ) | unaryOp )
+    private void assign() {
+        designator();
+        if (accept(NonTerminal.ASSIGN_OP)) {
+            relExpr();
+        } else if (accept(NonTerminal.UNARY_OP)) {
+            return;
+        } else {
+            expect(NonTerminal.ASSIGN);
+        }
+    }
+
+    // relExpr = addExpr { relOp addExpr }
+    private void relExpr () {
+        // for empty return statements
+        if(have(Token.Kind.SEMICOLON) || have(Token.Kind.CLOSE_PAREN)){
+            return;
+        }
+        addExpr();
+        while (accept(NonTerminal.REL_OP)){
+            addExpr();
+        }
+    }
+
+    // addExpr = multExpr { addOp multExpr }
+    private void addExpr () {
+        multExpr();
+        while (accept(Token.Kind.ADD) || accept(Token.Kind.SUB) || accept(Token.Kind.OR)) {
+            multExpr();
+        }
+    }
+
+    // multExpr = powExpr { multOp powExpr }
+    private void multExpr() {
+        powExpr();
+        while (accept(Token.Kind.MUL) || accept(Token.Kind.DIV) || accept(Token.Kind.MOD) || accept(Token.Kind.AND)) {
+            powExpr();
+        }
+    }
+
+    // powExpr = groupExpr { powOp groupExpr }
+    private void powExpr () {
+        groupExpr();
+        while (accept(Token.Kind.POW)) {
+            groupExpr();
+        }
+    }
+
+    // groupExpr = literal | designator | "not" relExpr | relation | funcCall
+    private void groupExpr() {
+        if (accept(NonTerminal.LITERAL)) {
+            //System.out.println("lit" + lineNumber());
+            return;
+        } else if (have(NonTerminal.DESIGNATOR)) {
+            // System.out.println("des" + lineNumber());
+            designator();
+        } else if (accept(Token.Kind.NOT)) {
+            // System.out.println("relExp" + lineNumber());
+            relExpr();
+        } else if (have(Token.Kind.OPEN_PAREN)) {
+            //System.out.println("realtion" + lineNumber());
+            relation();
+        } else if (have(NonTerminal.FUNC_CALL)){
+            //System.out.println("call" + lineNumber());
+            funcCall();
+        } else {
+            expect(NonTerminal.GROUP_EXPR);
+        }
+    }
+
+    // relation = "(" relExpr ")"
+    private void relation() {
+        expect(Token.Kind.OPEN_PAREN);
+        relExpr();
+        expect(Token.Kind.CLOSE_PAREN);
+    }
+
+    // funCall = "call" ident "(" [ relExpr { "," relExpr } ] ")"
+    private void funcCall() {
+        expect(Token.Kind.CALL);
+        expect(Token.Kind.IDENT);
+        expect(Token.Kind.OPEN_PAREN);
+        relExpr();
+        while (accept(Token.Kind.COMMA)) {
+            relExpr();
+        }
+        expect(Token.Kind.CLOSE_PAREN);
+    }
+
+    // ifStat = "if" relation "then" statSeq [ "else" statSeq ] "fi"
+    private void ifStat() {
+        expect(Token.Kind.IF);
+        relation();
+        expect(Token.Kind.THEN);
+        statSeq();
+        if (accept(Token.Kind.ELSE)) {
+            statSeq();
+        }
+        expect(Token.Kind.FI);
+    }
+
+    // whileStat = "while" relation "do" statSeq "od"
+    private void whileStat () {
+        expect(Token.Kind.WHILE);
+        relation();
+        expect(Token.Kind.DO);
+        statSeq();
+        expect(Token.Kind.OD);
+    }
+
+    // repeatStat = "repeat" statSeq "until" relation
+    private void repeatStat () {
+        expect(Token.Kind.REPEAT);
+        statSeq();
+        expect(Token.Kind.UNTIL);
+        relation();
+    }
+
+    // returnStat = "return" [ relExpr ]
+    private void returnStat () {
+        expect(Token.Kind.RETURN);
+        relExpr();
+    }
+
+
+
 }
